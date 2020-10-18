693ba513c5e420dfd78890e06a9e63184c3e5551
ersvgr <72547032+ersvgr@users.noreply.github.com>
2020-10-18 09:11:04 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -16,7 +16,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 {
     "inbounds": [
         {
-            "port": $PORT,
+            "port": 443,
             "protocol": "vless",
             "settings": {
                 "clients": [
